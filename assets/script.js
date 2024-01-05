99b7bb198b7d983fbf9d276e22fe67915c9d2e32
DomSimonetta <Guitardudedom@gmail.com>
2024-01-05 14:16:06 -0500

    added javascript

--- assets/script.js
+++ assets/script.js
@@ -6,4 +6,31 @@ const currentWeather = doccument.getElementById('currentWeather')
 
 function fetchWeatherData(city) {
     const weatherApiUrl = 'https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${weatherApiKey}&units=metric'
+    
+    fectch(weatherApiUrl)
+    .then(response => response.json())
+    .then(data => displayWeather(data))
 }
+
+function displayWeather(weatherData) {
+
+    currentWeather.innerHTML = '';
+
+    weatherCard.appendChild(weatherCity);
+    weatherCard.appendChild(weatherTemp);
+}
+
+submitButton.addEventListener('click', function (event) {
+    event.preventDefault();
+
+    var input =inputValue.value;
+    varinputValue = JSON.parse(localStorage.getitem('location'));
+    
+})
+
+//function saveHistory(city) {
+//cityHistory.appendChild;
+//}
+
+fetchWeatherData();
+displayWeather();
