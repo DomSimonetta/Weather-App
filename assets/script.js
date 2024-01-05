a668fe42490d37b25bcfa1a0da12172c0ce2d815
DomSimonetta <Guitardudedom@gmail.com>
2024-01-05 06:11:17 -0500

    added a readme and some simple java

--- assets/script.js
+++ assets/script.js
@@ -1,2 +1,9 @@
 const weatherApiKey = 'a435cdac1340cfbcac761f45e54d29ff';
 
+const inputLocation = document.getElementById('inputLoccation')
+const submitButton = document.getElementById('submitButton')
+const currentWeather = doccument.getElementById('currentWeather')
+
+function fetchWeatherData(city) {
+    const weatherApiUrl = 'https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${weatherApiKey}&units=metric'
+}
